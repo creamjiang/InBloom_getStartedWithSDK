258a036cd4eda2c9e57b864c972457f760ed55e8
Wes Williams <wwwestech@gmail.com>
2013-01-29 22:01:51 -0700

    Update the config file

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ var config = {};
 
 config.app = {};
 
-config.app.port = 8080;
+config.app.port = 3000;
 config.api = {};
 
 config.api.base_url = 'https://api.sandbox.slcedu.org';
-config.api.client_id = '$YOUR_CLIENT_ID_HERE'
-config.api.client_secret = '$YOUR_ENCRYPTED_CLIENT_SECRET_HERE';
-config.api.oauth_uri = 'http://$YOUR_IP_ADDRESS:$YOUR_PORT/callback';
-config.api.api_version = "$API_VERSION_HERE";
+config.api.client_id = 'Msdfs4gdFyd';
+config.api.client_secret = '34tfgdFdfDFGdfh3dfgeANKE';
+config.api.oauth_uri = 'http://127.0.0.1:3000/oauth';
+config.api.api_version = "v1";
 
 module.exports = config;
