e028ad65bc31406269ce93cd086dbae236a1d021
Wes Williams <wwwestech@gmail.com>
2013-02-02 16:32:51 -0700

    added obvious placeholders in place of bogus client data

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ config.app.port = 3000;
 config.api = {};
 
 config.api.base_url = 'https://api.sandbox.slcedu.org';
-config.api.client_id = 'Msdfs4gdFyd';
-config.api.client_secret = '34tfgdFdfDFGdfh3dfgeANKE';
+config.api.client_id = '**PutYourSlcClientIdHere**';
+config.api.client_secret = '**PutYourSlcClientSecretHere**';
 config.api.oauth_uri = 'http://127.0.0.1:3000/oauth';
 config.api.api_version = "v1";
 
